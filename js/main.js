408a6ab7822c2c93cdde392c224ea2896453d6e0
emran <likeeboyistekarofficial@gmail.com>
2024-02-05 13:14:16 +0600

    boilerplate

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// $(document).ready(function(){
+//     $('#slick-slider').slick({
+//         slidesToShow: 1,
+//         infinite: true,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         dots: false,
+//         arrows: true,
+//         pauseOnHover: false
+//     });
+//     $('#productsSlider').slick({
+//         slidesToShow: 6,
+//         infinite: true,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         dots: false,
+//         arrows: true,
+//         pauseOnHover: false,
+//         responsive: [
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     slidesToShow: 4,
+//                     slidesToScroll: 4,
+//                     infinite: true,
+//                     dots: false
+//                 }
+//             },
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     slidesToShow: 3,
+//                     slidesToScroll: 3,
+//                     infinite: true,
+//                     dots: false
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 2,
+//                     infinite: true,
+//                     dots: false
+//                 }
+//             },
+//             {
+//                 breakpoint: 576,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1,
+//                     infinite: true,
+//                     dots: false
+//                 }
+//             }
+//         ]
+//     });
+// });
