ae227724d0ae4e2869297dcdde31eef195b816a3
emran <likeeboyistekarofficial@gmail.com>
2024-02-28 00:50:59 +0600

    Boilerplate

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+    // $('.pet-detalis-group-header-left-photo-slider').slick({
+    //     slidesToShow: 1,
+    //     infinite: true,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     dots: false,
+    //     arrows: true,
+    //     pauseOnHover: false
+    // });
+
+//     $('.slider-for').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         fade: true,
+//         asNavFor: '.slider-nav'
+//     });
+
+//     $('.slider-nav').slick({
+//         slidesToShow: 5,
+//         slidesToScroll: 1,
+//         asNavFor: '.slider-for',
+//         dots: false,
+//         centerMode: true,
+//         arrows: false,
+//         focusOnSelect: true
+//     });
+
+//     $('.our-customer-slider').slick({
+//         slidesToShow: 5,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         dots: true,
+//     });
+// });
