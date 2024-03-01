f3b703b3bdd359a271508449743fabbed29749c1
emran <likeeboyistekarofficial@gmail.com>
2024-03-01 22:30:04 +0600

    Products Section

--- js/main.js
+++ js/main.js
@@ -1,37 +1,49 @@
 // $(document).ready(function(){
-    // $('.pet-detalis-group-header-left-photo-slider').slick({
-    //     slidesToShow: 1,
-    //     infinite: true,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     dots: false,
-    //     arrows: true,
-    //     pauseOnHover: false
-    // });
-
-//     $('.slider-for').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         arrows: true,
-//         fade: true,
-//         asNavFor: '.slider-nav'
-//     });
-
-//     $('.slider-nav').slick({
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         asNavFor: '.slider-for',
-//         dots: false,
-//         centerMode: true,
-//         arrows: false,
-//         focusOnSelect: true
-//     });
-
-//     $('.our-customer-slider').slick({
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         dots: true,
+//     $('#productsSlider').slick({
+//     slidesToShow: 3,
+//     infinite: false,
+//     slidesToScroll: 3,
+//     autoplay: true,
+//     dots: false,
+//     arrows: true,
+//     pauseOnHover: false,
+//     responsive: [
+//         {
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 4,
+//                 slidesToScroll: 4,
+//                 infinite: true,
+//                 dots: false
+//             }
+//         },
+//         {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: false
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//                 infinite: true,
+//                 dots: false
+//             }
+//         },
+//         {
+//             breakpoint: 576,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 infinite: true,
+//                 dots: false
+//             }
+//         }
+//     ]
 //     });
 // });
