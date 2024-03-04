9990d0cfa9c43147cd7c0cbdff7e653d4fe6b861
emran <likeeboyistekarofficial@gmail.com>
2024-03-04 11:11:58 +0600

    End Header Banner Section

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
 // $(document).ready(function(){
-    // $('.pet-detalis-group-header-left-photo-slider').slick({
-    //     slidesToShow: 1,
-    //     infinite: true,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     dots: false,
-    //     arrows: true,
-    //     pauseOnHover: false
-    // });
-
-//     $('.slider-for').slick({
+//     $('#header-banner-slider').slick({
 //         slidesToShow: 1,
+//         infinite: true,
 //         slidesToScroll: 1,
-//         arrows: true,
-//         fade: true,
-//         asNavFor: '.slider-nav'
-//     });
-
-//     $('.slider-nav').slick({
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         asNavFor: '.slider-for',
+//         autoplay: false,
 //         dots: false,
-//         centerMode: true,
-//         arrows: false,
-//         focusOnSelect: true
-//     });
-
-//     $('.our-customer-slider').slick({
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         dots: true,
+//         arrows: true,
+//         pauseOnHover: false
 //     });
 // });
